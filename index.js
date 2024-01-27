52336dbc78fb1b079f6e2edd079fa7144a6021f0
Gabriel Oliveira <goliveira182826@gmail.com>
2024-01-26 22:58:17 -0300

    a

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-function requestApi(searchTerm) {
-    console.log('Chamando requestApi');
-    const url = `https://gabrielkkskx.github.io/artists/artists.json?name_like=${searchTerm}`;
-    fetch(url)
-        .then((response) => response.json())
-        .then((result) => displayResults(result))
-        .catch((error) => console.error('Erro na requisição:', error));
-}
-
-function displayResults(result) {
-    console.log('Chamando displayResults');
-    // Restante do código...
-}
+console.log('funciona');
